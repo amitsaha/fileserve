13d357512a71f87232098154ee01f4d0e5435e55
Amit Saha <amitsaha.in@gmail.com>
2015-11-24 20:03:51 +1100

    dummy commit; doesn't work

--- fileserve.go
+++ fileserve.go
@@ -1,21 +1,17 @@
-/* 
+// Start a HTTP server serving the files in a directory.
 
-Start a HTTP server serving the files in a directory.
+// Usage:
 
-Usage:
+// $ go run fileserve.go -d <directory> -p <port>
 
-$ go run fileserve.go -d <directory> -p <port>
+// Examples:
 
-Examples:
+// By default, the files in the current directory will be served on port 8080:
+// $ go run fileserve.go 
 
-By default, the files in the current directory will be served on port 8080
+// To specify a different directory and port:
+// $ go run fileserve.go -d=/tmp -p=9090
 
-$ go run fileserve.go 
-
-To specify a different directory and port:
-
-$ go run fileserve.go -d=/tmp -p=9090
-*/
 package main
 
 import (
@@ -24,15 +20,24 @@ import (
 	"log"
 )
 
+type myType struct {}
+
+func (t *myType) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	log.Printf("Request recieved")
+	http.FileServer(http.Dir("/tmp"))
+}
+
 func main() {
 	port := flag.String("p", "8080", "Port to serve on")
-	directoryToServe := flag.String("d", ".", "Directory to serve")
+	//directoryToServe := flag.String("d", ".", "Directory to serve")
 	flag.Parse()	
 	listenOn := "127.0.0.1:" + *port
 	// ListenAndServe is blocking and runs each handler in a goroutine, so can serve multiple
 	// requests simultaneously
 	// http.FileServer: https://golang.org/pkg/net/http/#FileServer
-	err := http.ListenAndServe(listenOn, http.FileServer(http.Dir(*directoryToServe)));
+	myTypeh := &myType{}
+	http.Handle("/", myTypeh)
+	err := http.ListenAndServe(listenOn, nil) //http.FileServer(http.Dir(*directoryToServe)));
 	if (err != nil) {
 		log.Fatal("ListenAndServe", err)
 	}
